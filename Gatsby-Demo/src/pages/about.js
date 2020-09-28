6bb331892a92236f2ee4a6c870758a58e1c383e4
Srujan Bethi <ssbethi@gmail.com>
2020-09-28 14:43:11 -0600

    Added about and contact pages

---
+++ Gatsby-Demo/src/pages/about.js
@@ -0,0 +1,18 @@
+//Create an about page. include page title and bio.
+
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Hello, my name is Srujan and I am a Full-Stack software engineer living in Boulder, Colorado. In my free time, I like to play and <br />
+        listen to music, usually anything that grooves. I also like to climb on the number of boulders and rock walls we have in the area, <br />
+        including Flagstaff, Flatirons, and Boulder Canyon. I also like to ride my mountain bike and road bike. In the winters, I take up <br />
+        snowboarding as my main form of activity. It's a fun area to be in!
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ Gatsby-Demo/src/pages/contact.js
@@ -0,0 +1,20 @@
+//Create contact page. Include page title and contact details (fake obviously, can't have people bombing my number)
+
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Details</h1>
+      <p>Name: Srujan Bethi<br />
+        Phone: 202-456-1111<br />
+        Email: fakeemail@fakemail.com<br />
+        Address: <br />The White House<br />
+                 1600 Pennsylvania Avenue NW<br />
+                 Washington, DC 20500
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
